818f862e813171832a8781fa056e769382fda8ed
Vikram Sundaram <vik.sundaram@hotmail.com>
2015-05-18 09:49:09 -0400

    Scanning and Parsing completed

--- src/otc/drivers/Main.java
+++ src/otc/drivers/Main.java
@@ -7,19 +7,26 @@ import otc.prettyprinter.PrettyPrinter;
 import otc.node.*; 
 
 import java.io.*;
+import java.util.Iterator;
 
 public class Main 
 {
 	public static void main(String[] args)
 	{
+		
+		try {
+			System.setIn(new FileInputStream("TestFiles/Valid/test1.onc"));
+		} catch (FileNotFoundException e1) {
+			System.out.println(System.getProperty("user.dir")); 
+			e1.printStackTrace();
+		}
+		
 		Parser p = new Parser(new Lexer(new PushbackReader(new InputStreamReader(System.in), 1024)));
 		
 		try {
-			Start tree = p.parse();
-			System.out.println("Done Parsing"); 
-			System.out.println(tree.toString()); 
+			Start tree = p.parse(); 
 			
-			
+			System.out.println(tree);			
 		} catch (ParserException e) {
 			System.out.println(e.toString());
 			e.printStackTrace();
